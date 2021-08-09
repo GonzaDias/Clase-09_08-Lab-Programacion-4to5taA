7b386a842c981636573e26b52ad41a4fe3dbf4f1
GonzaDias <83427315+GonzaDias@users.noreply.github.com>
2021-08-09 11:39:26 -0300

    Add files via upload

---
+++ condicionalIF.py
@@ -0,0 +1,29 @@
+suma = 0
+contador = 0
+for x in range(5):
+    nota = int(input("Ingrese la nota: "))
+    if nota >5:
+        suma = nota + suma
+        contador +=1
+    
+print("La suma es: ",suma)
+promedio = suma/contador
+print("el promedio es: ",promedio)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+        
